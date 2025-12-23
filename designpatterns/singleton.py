1cfb2665355b0369447e312190cdef6e8d33b72f
Vaibhav Gupta <vaibhav.gupta3659@gmail.com>
2025-12-23 06:31:12 +0530

    changed folder location in my local machine

--- designpatterns/singleton.py
+++ designpatterns/singleton.py
@@ -1,17 +1,17 @@
-# class Singleton:
-#     _instance = None
+class Singleton:
+    _instance = None
 
-#     def __new__(cls, *args, **kwargs):
-#         if cls._instance is None:
-#             cls._instance = super().__new__(cls)
-#         return cls._instance
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
 
-# # ----------- Usage -------------
-# if __name__ == "__main__":
-#     s1 = Singleton()
-#     s2 = Singleton()
+# ----------- Usage -------------
+if __name__ == "__main__":
+    s1 = Singleton()
+    s2 = Singleton()
 
-#     print(s1 is s2)  # True
+    print(s1 is s2)  # True
 
 
 # __init__ is an instance method, while __new__ is a static method.
@@ -20,18 +20,18 @@
 # __new__ happens first, then __init__.
 # __new__ can return any object, while __init__ must return None.
 
-# class Singleton:
-#     _instance = None
+class Singleton:
+    _instance = None
     
-#     def __new__(cls):
-#         if cls._instance is None:
-#             cls._instance = super().__new__(cls)
-#         return cls._instance
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
     
     
 
-# if __name__ == "__main__":
-#     s1 = Singleton()
-#     s2 = Singleton()
+if __name__ == "__main__":
+    s1 = Singleton()
+    s2 = Singleton()
     
-#     print(s1 is s2)
+    print(s1 is s2)
